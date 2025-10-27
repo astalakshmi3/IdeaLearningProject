87bd22403e2a1cffa248696d2c83b3b65f6e17ee
Astalakshmi <astalskshmimuralidharan@gmail.com>
2025-10-27 22:21:15 +0530

    Initial commit

---
+++ src/Welcome.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Welcome {
+    public static void main(String[] args) {
+        int[] array = {5, 6, 7, 8};
+        System.out.println("AVERAGE of array " + Arrays.toString(array) + " is " + findAverage(array));
+    }
+
+    private static double findAverage(int[] values) {
+        double result = 0;
+
+        for (int value : values) {
+            result += value;
+        }
+        return result / values.length;
+    }
+}
